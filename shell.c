647b1a0dea91fbd815d8959300bc822e9128016f
AfyaNarh <nhyiraafya1@gmail.com>
2023-06-26 04:36:19 -0700

    simple shell

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * main - starting point
+ * @ac: argument to be counted
+ * @av: vector of the argument
+ * @env: environment variables
+ * Return: 0
+ */
+int main(int ac, char **av, char **env)
+{
+        if (ac == 1)
+                prompt_command(av, env);
+        return (0);
+}
+
---
+++ shell.h
@@ -0,0 +1,28 @@
+shell.h
+#ifndefHELL_H_
+#defineHELL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stddef.h>
+
+
+#define BUF_SIZE 1024
+#define Max_Command 20
+
+
+void prompt_command(char **av, char **env);
+int main(int ac, char **av, char **env);
+void check_path(const char *command);
+int _strlen(const char *s);
+int _strcmp(const char *st1, const char *st2);
+void execute_command(char *command, char **env);
+
+#endif
+
+
