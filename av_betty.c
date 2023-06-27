0306c8cb402c9bf40195fd039dbbcd00cd25df30
Akos-1 <veronicagyasi@gamil.com>
2023-06-27 13:17:21 -0700

    Update

--- av_betty.c
+++ av_betty.c
@@ -14,16 +14,33 @@ int sum_numb(int a, int v)
  * main - Entry point.
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int num1 = 9;
 	int num2 = 11;
 	int result = sum_numb(num1, num2);
+
 	char buffer[20];
+	int length = 0;
+	int temp = result;
 
-	int length = snprintf(buffer, sizeof(buffer), "The sum is: %d\n", result);
+	do {
+		temp /= 10;
+		length++;
+	} while (temp != 0);
 
+	int i = length;
+
+	while (result != 0)
+	{
+		buffer[--i] = '0' + (result % 10);
+		result /= 10;
+	}
+
+	write(STDOUT_FILENO, "The sum is: ", 12);
 	write(STDOUT_FILENO, buffer, length);
+	write(STDOUT_FILENO, "\n", 1);
+
 	return (0);
 }
+
