23031a3300925093d3cabfc002d2c91ec830179d
Akos-1 <veronicagyasi@gamil.com>
2023-06-28 07:25:04 -0700

    New Update

--- avexecute_command.c
+++ avexecute_command.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * execute_command - Execute a shell command
  * @command: The command to execute
@@ -7,21 +8,22 @@
 void execute_command(char *command, char **env)
 {
 	int status;
-	char *argv[2];
 	pid_t pid = fork();
 
 	if (pid == -1)
 	{
-		write(STDERR_FILENO, "Error: Fork failed\n", 19);
+		perror("Error: Fork failed");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
+		char *argv[2];
+
 		argv[0] = command;
 		argv[1] = NULL;
 		if (execve(command, argv, env) == -1)
 		{
-			write(STDERR_FILENO, "Error: Execve failed\n", 21);
+			perror("Error: Execve failed");
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -29,9 +31,9 @@ void execute_command(char *command, char **env)
 	{
 		if (waitpid(pid, &status, 0) == -1)
 		{
-			write(STDERR_FILENO, "Error: Waitpid failed\n", 22);
+			perror("Error: Waitpid failed");
 			exit(EXIT_FAILURE);
 		}
 	}
-
 }
+
