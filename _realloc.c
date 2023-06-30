97954e20a232beae9c6618c18d73f7523744535f
Akos-1 <veronicagyasi@gamil.com>
2023-06-30 11:05:51 -0700

    Updated

--- _realloc.c
+++ _realloc.c
@@ -4,33 +4,23 @@
 
 /**
  * my_realloc - Reallocates a memory block with a new size
- * @ptr: Pointer to the previously allocated memory block
- * @size: New size for the memory block
- *
+ * @prev_size: previously allocated memory block
+ * @ptr: pointer to previously allocated memory block
+ * @cur_size: New size for the memory block
  * Return: Pointer to the reallocated memory block, or NULL on failure
  */
-void *my_realloc(void *ptr, size_t size)
+void *my_realloc(void *ptr, unsigned int prev_size, unsigned int cur_size)
 {
-	void *new_ptr;
-
-	new_ptr = malloc(size);
-	if (new_ptr == NULL)
+	if (ptr != NULL && cur_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (ptr != NULL)
-	{
-		size_t old_size = malloc_usable_size(ptr);
-
-		if (size < old_size)
-			old_size = size;
-
-		memcpy(new_ptr, ptr, old_size);
-		free(ptr);
-	}
-
-	return (new_ptr);
+	if (ptr == NULL)
+		ptr = malloc(cur_size);
+	if (cur_size == prev_size)
+		return (ptr);
+	free(ptr);
+	ptr = malloc(cur_size);
+	return (ptr);
 }
-
--- avgetline.c
+++ avgetline.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
+#include "shell.h"
 /**
  * our_getline - Reads a line of input from a file stream
  * @linebuffer: Pointer to the buffer containing the line
